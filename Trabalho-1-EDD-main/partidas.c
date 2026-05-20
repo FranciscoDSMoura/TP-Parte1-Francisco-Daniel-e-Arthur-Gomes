d536e74a786e7dcc8c7cef13e856f39de7f3f68e
FranciscoDSMoura <franciscodsmoura@gmail.com>
2026-05-20 11:01:01 -0300

    Add files via upload

---
+++ Trabalho-1-EDD-main/partidas.c
@@ -0,0 +1,2 @@
+#include "partidas.h"
+
---
+++ Trabalho-1-EDD-main/partidas.h
@@ -0,0 +1,12 @@
+#ifndef PARTIDAS_H
+#define PARTIDAS_H
+
+typedef struct {
+    int id;           
+    int id_time1;     
+    int id_time2;     
+    int gols_time1;   
+    int gols_time2;   
+} Partida;
+
+#endif
